0c8dee947aa2fbc89efa55eb7ecd0cba8d0ec925
Rokas Jankunas <hello@rokasjankunas.com>
2019-11-05 16:34:18 +0000

    14 & 15 (UNTESTED)
    Move to testing after merging

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ using namespace std;
 vector<bool> ruleSet;
 vector<bool> prevGen;
 vector<bool> currGen;
+vector<vector<bool>> prevGen2d; //[row][column]
+vector<vector<bool>> currGen2d;
 int generationsPerformed = 0;
 string outputFileName = "output.txt";
 string inputFileName = "input.txt";
@@ -24,16 +26,16 @@ void saveFile();
 void loadFile();
 
 int main() {
-  	loadFile();
+  	// loadFile();
 
-	// ruleSet.push_back(0);
-	// ruleSet.push_back(0);
-	// ruleSet.push_back(0);
-	// ruleSet.push_back(1);
-	// ruleSet.push_back(1);
-	// ruleSet.push_back(1);
-	// ruleSet.push_back(1);
-	// ruleSet.push_back(0);
+	ruleSet.push_back(0);
+	ruleSet.push_back(0);
+	ruleSet.push_back(0);
+	ruleSet.push_back(1);
+	ruleSet.push_back(1);
+	ruleSet.push_back(1);
+	ruleSet.push_back(1);
+	ruleSet.push_back(0);
   
  //    int size = firstGenerationSize();
     int generations = numberOfGenerationsToRun();
@@ -44,17 +46,49 @@ int main() {
 		printGeneration1d();
 	}
 
-	saveFile();
+	// saveFile();
     return 0;
 }
 
 void performGeneration1d(bool wrap) {
 	generationsPerformed++;
+
 	currGen[0] = ruleSet[getRuleIndex(wrap ? prevGen[currGen.size() - 1] : false, prevGen[0], prevGen[1])];
 	for (int i = 1; i < currGen.size() - 1; i++) {
 		currGen[i] = ruleSet[getRuleIndex(prevGen[i - 1], prevGen[i], prevGen[i + 1])];
 	}
 	currGen[currGen.size() - 1] = ruleSet[getRuleIndex(prevGen[currGen.size() - 2], prevGen[currGen.size() - 1], wrap ? prevGen[0] : false)];
+
+	prevGen = currGen;
+}
+
+void performGeneration2d(bool wrap) {
+	generationsPerformed++;
+
+	//top row (done separately to allow wrapping)
+	if (wrap) {
+		currGen2d[0][0] = ruleSet[getRuleIndex(prevGen2d[currGen2d.size() - 1][currGen2d[0].size()-1], prevGen2d[currGen2d.size() - 1][0], prevGen2d[currGen2d.size() - 1][1])];
+		for (int i = 1; i < currGen2d[0].size() - 1; i++) {
+			currGen2d[0][i] = ruleSet[getRuleIndex(prevGen2d[currGen2d.size() -1][i - 1], prevGen2d[currGen2d.size() -1][i], prevGen2d[currGen2d.size() -1][i + 1])];
+		}
+		currGen2d[0][currGen2d[0].size() - 1] = ruleSet[getRuleIndex(prevGen2d[0][currGen2d[0].size() - 2], prevGen2d[0][currGen2d[0].size() -1], prevGen2d[0][0])];
+	} else {
+		currGen2d[0][0] = ruleSet[getRuleIndex(false, false, false)];
+		for (int i = 1; i < currGen2d[0].size() - 1; i++) {
+			currGen2d[0][i] = ruleSet[getRuleIndex(false, false, false)];
+		}
+		currGen2d[0][currGen2d[0].size() - 1] = ruleSet[getRuleIndex(false, false, false)];
+	}
+
+	//other rows
+	for (int i = 1; i < currGen2d.size(); i++) {
+		currGen2d[i][0] = ruleSet[getRuleIndex(wrap ? prevGen2d[i - 1][currGen2d[0].size() - 1] : false, prevGen2d[i - 1][0], prevGen2d[i - 1][1])];
+		for (int j = 1; j < currGen2d[0].size() - 1; j++) {
+			currGen2d[i][j] = ruleSet[getRuleIndex(prevGen2d[i - 1][j - 1], prevGen2d[i - 1][j], prevGen2d[i - 1][j + 1])];
+		}
+		currGen2d[i][currGen2d[0].size() - 1] = ruleSet[getRuleIndex(prevGen2d[i - 1][currGen2d[0].size() - 2], prevGen2d[i - 1][currGen2d[0].size() - 1], wrap ? prevGen2d[i - 1][0] : false)];
+	}
+
 	prevGen = currGen;
 }
 
